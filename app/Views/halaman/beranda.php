27a0c80dc2cc7a6b29b6c9efb8852913612c4084
aderahul <ripmy944@gmail.com>
2021-10-03 20:33:13 +0700

    tugas pertemuan3

--- app/Views/halaman/beranda.php
+++ app/Views/halaman/beranda.php
@@ -1,1 +1,18 @@
-<?php
+<?=$this->extend('dasar_tampilan')?>
+
+<?=$this->section('isiweb')?>
+
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h3 class="card-title">Beranda</h3>
+            </div>
+            <div class="card-body">
+                Halo ini beranda<br/>
+                Email yang di isikan : <?=$email?> <br/>
+                Password : <?=$sandi?>
+            </div>
+        </div>
+    </div>
+
+<?=$this->endSection()?>
