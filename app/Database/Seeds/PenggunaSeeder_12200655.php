76911cd70514d039fe425581fa0d1bb7db8d5029
aderahul <ripmy944@gmail.com>
2021-10-09 17:33:25 +0700

    Tugas Pertemuan4

---
+++ app/Database/Seeds/PenggunaSeeder_12200655.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\Database\Seeds;
+
+use App\Models\PenggunaModel_12200655;
+use CodeIgniter\Database\Seeder;
+
+class PenggunaSeeder_12200655 extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'nama'        => 'aderahul',
+                'password'    => md5('12200655')
+            ],
+            [
+                'nama'        => 'admin',
+                'password'    => md5('12345')
+            ],
+            [
+                'nama'        => '12200655_aderahul',
+                'password'    => md5('aderahul')
+            ]
+        ];
+
+        $p = new PenggunaModel_12200655();
+        $p->insertBatch($data);
+    }
+}
